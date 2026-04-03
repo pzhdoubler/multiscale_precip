af6f621f93aa80dfe536525b536ddeccec62789c
pzhdoubler <phdoubler01@gmail.com>
2026-04-03 17:30:44 -0400

    added median and 95th

--- precip_explore.py
+++ precip_explore.py
@@ -13,10 +13,12 @@ def get_precip_stats(path, file):
 
     t = dt_obj.strftime("%Y-%m-%d %H:%M")
     avg = var.mean().item()
+    median = np.median(var.values)
+    nf_per = np.percentile(var.values, 95)
     max = var.max().item()
     min = var.min().item()
     sum = var.sum().item()
-    return [file, t, avg, max, min, sum]
+    return [t, avg, median, nf_per, max, min, sum]
 
 # Path to your NetCDF files
 mswep_path = "/ocean/projects/ees210011p/hdoubler/AOSC650/mswep/trimmed/"
@@ -26,48 +28,20 @@ files = files[:10]
 
 mswep_files = [(mswep_path, f) for f in files]
 
-print(files)
-
 with Pool() as pool:
-    # file, time, avg, max, min, sum
+    # [time, avg, max, min, sum]
     stats = np.asarray(pool.starmap(get_precip_stats, mswep_files))
     print(stats.shape)
     print("Stats done. Saving...")
     df = pd.DataFrame({
-        'file': stats[:,0],
         'time': stats[:,1],
         'mean': stats[:,2],
-        'max': stats[:,3],
-        'min': stats[:,4],
-        'sum': stats[:,5]
+        'median': stats[:,3],
+        '95th': stats[:,4],
+        'max': stats[:,5],
+        'min': stats[:,6],
+        'sum': stats[:,7]
     })
     # Save to CSV
     df.to_csv("/ocean/projects/ees210011p/hdoubler/AOSC650/mswep/trimmed_precip_stats.csv", index=False)
     print("Saved statistics to trimmed_precip_stats.csv")
-
-
-# plt.figure()
-# plt.hist(means, bins=50)
-# plt.title("Mean Precipitation")
-# plt.xlabel("Value")
-# plt.ylabel("Frequency")
-# plt.show()
-
-# plt.figure(figsize=(8,5))
-# plt.hist(means, bins=100, range=(0, np.percentile(means, 50)))  # zoom on lower half
-# plt.title("Zoomed-in Histogram of Precipitation (lower end)")
-# plt.xlabel("Precipitation value")
-# plt.ylabel("Frequency")
-# plt.show()
-
-# plt.figure(figsize=(8,5))
-# plt.hist(means, bins=100, range=(0, np.percentile(means, 90)), log=True)
-# plt.title("Histogram of Precipitation (log scale)")
-# plt.xlabel("Precipitation value")
-# plt.ylabel("Log frequency")
-# plt.show()
-
-# plt.figure()
-# plt.hist(maxs, bins=50)
-# plt.title("Max Precipitation")
-# plt.show()
