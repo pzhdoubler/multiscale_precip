fbe6e00c0493d496193b91ceb003af8de752606d
pzhdoubler <phdoubler01@gmail.com>
2026-04-03 15:30:39 -0400

    added multiprocessing

--- trim_data.py
+++ trim_data.py
@@ -1,12 +1,10 @@
 import os
 import numpy as np
 import xarray as xr
-import time
+from multiprocessing import Pool
 
-def slice_mswep_with_xu_wrf(coords_loc, mswep_loc, mswep_save_loc):
+def slice_mswep_with_xu_wrf(ds_coords, file, save):
     ##### GET MSWEP LAT/LON SLICE
-    ds_coords = xr.open_dataset(coords_loc + "xu_wrf_coordinates.nc")
-
     lon_buffer = 0.2
     lat_buffer = 0.2
     pr_lat_min = np.min(ds_coords["lat"].values) - lat_buffer
@@ -14,45 +12,9 @@ def slice_mswep_with_xu_wrf(coords_loc, mswep_loc, mswep_save_loc):
     pr_lon_min = np.min(ds_coords["lon"].values) - lon_buffer
     pr_lon_max = np.max(ds_coords["lon"].values) + lon_buffer
 
-    # xu_lat_min = np.min(ds_coords["lat"].values)
-    # xu_lat_max = np.max(ds_coords["lat"].values)
-    # xu_lon_min = np.min(ds_coords["lon"].values)
-    # xu_lon_max = np.max(ds_coords["lon"].values)
-
-    # print("LATS:", xu_lat_min, xu_lat_max)
-    # print("LONS:", xu_lon_min, xu_lon_max)
-
-    # print("BUFF LATS:", pr_lat_min, pr_lat_max)
-    # print("BUFF LONS:", pr_lon_min, pr_lon_max)
-
-    # list mswep files
-    mswep = os.listdir(mswep_loc)
-    init = True
-
-    ds = xr.open_mfdataset(
-        os.path.join(mswep_loc, "*.nc"),
-        combine="by_coords",
-        parallel=True,
-        chunks={"lat": 200, "lon": 200}
-    )
-
-    subset = ds.sel(
-        lat=slice(pr_lat_max, pr_lat_min),
-        lon=slice(pr_lon_min, pr_lon_max)
-    )
-
-    subset.to_netcdf("combined_subset.nc")
-
-    # for file in mswep:
-    #     s = time.time()
-    #     ds = xr.open_dataset(file)
-    #     subset = ds.sel(lat=slice(pr_lat_max, pr_lat_min), lon=slice(pr_lon_min, pr_lon_max))
-    #     subset.to_netcdf(mswep_save_loc + file)
-    #     if init:
-    #         init = False
-    #         e = time.time()
-    #         print(f"One file took {e - s} seconds")
-    #         print(f"All files estimated to take {len(mswep) * (e - s)} seconds")
+    ds = xr.open_dataset(file)
+    subset = ds.sel(lat=slice(pr_lat_max, pr_lat_min), lon=slice(pr_lon_min, pr_lon_max))
+    subset.to_netcdf(save)
 
 
 
@@ -60,4 +22,11 @@ coords_loc = "../"
 mswep_loc = "../mswep/hourly/"
 mswep_save = "../mswep/trimmed/"
 
-slice_mswep_with_xu_wrf(coords_loc, mswep_loc, mswep_save)
+xu_wrf = xr.open_dataset(coords_loc + "xu_wrf_coordinates.nc")
+mswep = os.listdir(mswep_loc)
+mswep = mswep[:5]
+mswep_tasks = [(xu_wrf, os.path.join(mswep_loc, f), os.path.join(mswep_save, f)) for f in mswep]
+
+with Pool() as pool:
+    done = pool.starmap(slice_mswep_with_xu_wrf, mswep_tasks)
+    print("Done.")
