20761641ba6d5a9c31a2edc089931f7618bc5062
pzhdoubler <phdoubler01@gmail.com>
2026-04-05 11:25:40 -0400

    remove imports

--- train.py
+++ train.py
@@ -3,10 +3,6 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-import keras
-from keras import layers
 from minisom import MiniSom
 
 def make_gp_anomalies(ds, method="monthly"):
