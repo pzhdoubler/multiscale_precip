7d5f64c809ff9e2321842c2d73e07e80892f2478
pzhdoubler <phdoubler01@gmail.com>
2026-04-05 11:14:34 -0400

    move preprocess to train

--- train.py
+++ train.py
@@ -9,3 +9,33 @@ import keras
 from keras import layers
 from minisom import MiniSom
 
+def make_gp_anomalies(ds, method="monthly"):
+    # simple monthly clim
+    if method == "monthly":
+        climatology = ds.groupby("time.month").mean("time")
+
+    anomalies = ds.groupby("time.month") - climatology
+
+    return anomalies
+
+########################
+###### PREPROCESS ######
+########################
+
+# gp open and anom calc
+gp_loc = "/ocean/projects/ees210011p/hdoubler/AOSC650/era5/coarse/"
+files = ["gp_500mb_2020.nc", "gp_500mb_2021.nc", "gp_500mb_2022.nc", "gp_500mb_2023.nc"]
+
+print("Opening gp data ...")
+ds = xr.open_mfdataset([os.path.join(gp_loc, f) for f in files])
+
+print("Calculating gp anoms ...")
+gp_anoms = make_gp_anomalies(ds, method="monthly")
+
+# determine precip days
+df = pd.read_csv("/ocean/projects/ees210011p/hdoubler/AOSC650/mswep/trimmed_precip_stats2.csv", parse_dates=["time"]).set_index("time")
+filtered_df = df[(df["drizzle exceedance"] > 0.05)]
+
+times = filtered_df.index
+gp_subset = gp_anoms.sel(time=times)
+print(gp_subset)
