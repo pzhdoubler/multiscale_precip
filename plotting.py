1bbc4b8282916cfbc9e896ef618a8e856921fc73
pzhdoubler <phdoubler01@gmail.com>
2026-04-06 16:23:24 -0400

    commenting

--- plotting.py
+++ plotting.py
@@ -15,25 +15,25 @@ lon = ds["lon"]
 lat = ds["lat"]
 time = ds["time"]
 
-lon2d, lat2d = np.meshgrid(lon, lat)
+# lon2d, lat2d = np.meshgrid(lon, lat)
 
-fig = plt.figure(figsize=(10, 8))
-ax = plt.axes(projection=ccrs.PlateCarree())
+# fig = plt.figure(figsize=(10, 8))
+# ax = plt.axes(projection=ccrs.PlateCarree())
 
-mesh = ax.contourf(
-    lon2d, lat2d, ds["precipitation"].sel(time=time).values[0],
-    transform=ccrs.PlateCarree()
-)
+# mesh = ax.contourf(
+#     lon2d, lat2d, ds["precipitation"].sel(time=time).values[0],
+#     transform=ccrs.PlateCarree()
+# )
 
-# Add features
-ax.add_feature(cfeature.COASTLINE)
-ax.add_feature(cfeature.BORDERS, linestyle=':')
-ax.add_feature(cfeature.STATES, edgecolor='gray', linewidth=0.8)
+# # Add features
+# ax.add_feature(cfeature.COASTLINE)
+# ax.add_feature(cfeature.BORDERS, linestyle=':')
+# ax.add_feature(cfeature.STATES, edgecolor='gray', linewidth=0.8)
 
-ax.set_extent([-108.0, -103.0, 38.0, 41.0], ccrs.PlateCarree())
+# ax.set_extent([-108.0, -103.0, 38.0, 41.0], ccrs.PlateCarree())
 
-plt.show()
-plt.close()
+# plt.show()
+# plt.close()
 
 ds = xr.open_dataarray("/ocean/projects/ees210011p/hdoubler/AOSC650/mswep/trimmed_annual/pr_2020.nc")
 
